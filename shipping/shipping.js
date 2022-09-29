47df27875778ee261ce21cb11f2ef44e3badb3df
harishpampana7 <harishpampana7675@gmail.com>
2022-09-29 23:02:46 +0530

    day3

---
+++ shipping/shipping.js
@@ -0,0 +1,16 @@
+
+
+let total = JSON.parse(localStorage.getItem("HP_checkout_total"));
+
+let Sub = document.querySelector("#HP_ST");
+Sub.innerText = "$ "+Number(total);
+
+let red = document.querySelector("#red");
+let x = Number(total)+Number(80);
+red.innerText = "$ "+x
+
+localStorage.setItem("HP_final_Total",JSON.stringify(x))
+
+document.querySelector("#HP_Place_order_btn").addEventListener("click",function(){
+   window.location.href="./payments/payments.html"
+})
---
+++ sign page/signup.js
@@ -0,0 +1,22 @@
+
+
+submit=()=>{
+
+    function Obj(text,email,password){
+    this.name=text;
+    this.email=email;
+    this.password=password;
+    
+    }
+    
+    let text=document.querySelector("#HP_name").value;
+    let email=document.querySelector("#HP_email").value
+    let password=document.querySelector("#HP_password").value
+    
+    let HP_Data=new Obj(text,email,password)
+    console.log(HP_Data)
+    
+    localStorage.setItem("HP_user",JSON.stringify(HP_Data))
+    window.location.href="signin.html"
+    }
+   
